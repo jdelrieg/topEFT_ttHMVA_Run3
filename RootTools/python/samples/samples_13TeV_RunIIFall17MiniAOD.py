a1d158d5162be2f90d46e12fe41d54bc794a8228
Theodor Christian Herwig <herwig@cern.ch>
2020-04-21 04:22:09 +0200

    add n2n1 c1n2 higgsino miniaod

--- RootTools/python/samples/samples_13TeV_RunIIFall17MiniAOD.py
+++ RootTools/python/samples/samples_13TeV_RunIIFall17MiniAOD.py
@@ -505,9 +505,13 @@ ZHTobb_ll, ZHToTauTau,
 ##SUSY SOS signals
 #TChiWZ
 SMS_TChiWZ=kreator.makeMCComponent("SMS_TChiWZ","/SMS-TChiWZ_ZToLL_mZMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_GridpackScan_94X_mc2017_realistic_v14-v1/MINIAODSIM","CMS",".*root",1)
+SMS_N2N1_higgsino=kreator.makeMCComponent("SMS_N2N1_higgsino","/SMS-N2N1-higgsino_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_GridpackScan_94X_mc2017_realistic_v14-v1/MINIAODSIM","CMS",".*root",1)
+SMS_N2C1_higgsino=kreator.makeMCComponent("SMS_N2C1_higgsino","/SMS-N2C1-higgsino_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_GridpackScan_94X_mc2017_realistic_v14-v1/MINIAODSIM","CMS",".*root",1)
 ##FIXME! add other signal models
 SignalSUSY = [
-SMS_TChiWZ
+    SMS_TChiWZ,
+    SMS_N2N1_higgsino,
+    SMS_N2C1_higgsino,
 ]
 
 # low Mll samples
