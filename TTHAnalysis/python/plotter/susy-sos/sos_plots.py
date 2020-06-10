0af0aa5eb13b89bd2210387c5faf3f712220c3da
vtavolar <vittorio.tavolaro@cern.ch>
2020-06-10 17:17:34 +0200

    attaching isr friends only for 2016; use flat norm syst for 2017 and 2018 isr

--- TTHAnalysis/python/plotter/susy-sos/sos_plots.py
+++ TTHAnalysis/python/plotter/susy-sos/sos_plots.py
@@ -71,7 +71,9 @@ P0="root://eoscms.cern.ch//eos/cms/store/cmst3/group/tthlep/peruzzi/NanoTrees_SO
 if args.inputDir: P0=args.inputDir+'/'
 nCores = args.nCores
 TREESALL = " --Fs {P}/recleaner --FMCs {P}/bTagWeights --FMCs {P}/jetmetUncertainties -P "+P0+"%s "%(YEAR)+"--readaheadsz 20000000 "
-TREESALLSKIM = TREESALL + " --FMCs {P}/signalWeights --FMCs {P}/isrWeights "
+TREESALLSKIM = TREESALL + " --FMCs {P}/signalWeights "
+if YEAR == "2016":
+    TREESALLSKIM = TREESALLSKIM + " --FMCs {P}/isrWeights "
 
 def base(selection):
     plotting=''
